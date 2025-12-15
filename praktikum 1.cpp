22d6a8b1c7592c35493bc79477419b531261c825
al-azhar18 <mnashrilaziz@gmail.com>
2025-12-15 11:48:34 +0700

    Add files via upload

---
+++ praktikum 1.cpp
@@ -0,0 +1,44 @@
+// praktikum 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float harga, diskon, totalDiskon, totalBayar;
+
+    
+    cout << "SELAMAT DATANG DI TOKO UNISBA BLITAR" << endl;
+    cout << "===================================" << endl << endl;
+   
+    cout << "Silakan masukkan:" << endl;
+    cout << "Masukkan harga barang  (Rp) = ";
+    cin >> harga;
+
+    cout << "diskon barang           (%) =  ";
+    cin >> diskon;
+
+   
+    totalDiskon = harga * diskon / 100;
+    totalBayar = harga - totalDiskon;
+
+    
+    cout << endl;
+    cout << "Jumlah diskon           = Rp " << totalDiskon << endl;
+    cout << "harga setelah diskon    = Rp " << totalBayar << endl;
+
+    return 0;
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
